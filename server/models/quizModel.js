2ba019a16e58aa50c63d98f4293f1025be95589e
unknown <liethdallal@gmail.com>
2024-03-10 18:51:53 -0500

    added the quiz model schema

---
+++ server/models/quizModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const quizSchema = new Schema({
+
+
+}, {timestamps: true})
+
+module.exports = mongoose.model('Quiz', quizSchema)
