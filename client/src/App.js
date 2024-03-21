3919849685aad1861beb020551edd7079fadb9f8
unknown <liethdallal@gmail.com>
2024-03-20 23:09:58 -0500

    Data is being fetched front end and backend are now connected

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
+import {useEffect, useState} from 'react'
 
 function App() {
+  const [quizes, setQuizes] = useState([])
+
+  useEffect( () => {
+    const fetchQuizes = async () =>{
+
+    const response = await fetch('/api/quizes')
+    const data = await response.json() 
+
+    if(response.ok){
+      setQuizes(data)
+    }
+  }
+  fetchQuizes()
+  }, [])
   return (
     <div className="App">
-      Testing 
+            <h1>Quizes</h1>
+            <ul>{quizes.map((quiz, index) => (
+              <div key={index}>
+                <li>{quiz.title}</li> 
+                <li>{quiz.description}</li>
+              </div>
+              
+  ))}
+  
+            </ul>
     </div>
   );
 }
