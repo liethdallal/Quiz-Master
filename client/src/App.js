fed840779801622c995441ac5180ae49f7b0966c
unknown <liethdallal@gmail.com>
2024-03-25 15:09:21 -0500

    quizes are now rendered in their own component and found a way to acess the questions and responses

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,10 @@
-import {useEffect, useState} from 'react'
+import Quizes from "./components/Quizes"
 
 function App() {
-  const [quizes, setQuizes] = useState([])
 
-  useEffect( () => {
-    const fetchQuizes = async () =>{
-
-    const response = await fetch('/api/quizes')
-    const data = await response.json() 
-
-    if(response.ok){
-      setQuizes(data)
-    }
-  }
-  fetchQuizes()
-  }, [])
   return (
-    <div className="App">
-            <h1>Quizes</h1>
-            <ul>{quizes.map((quiz, index) => (
-              <div key={index}>
-                <li>{quiz.title}</li> 
-                <li>{quiz.description}</li>
-              </div>
-              
-  ))}
-  
-            </ul>
+    <div className="app-div">
+      <Quizes/>
     </div>
   );
 }
