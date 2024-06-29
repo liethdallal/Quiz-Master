ec5756e492abd992a73c1250dfd7e5da865911aa
unknown <liethdallal@gmail.com>
2024-06-28 21:37:24 -0500

    user can now select options under each question

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import '../styling/quiz.css' 
 
 function Quiz() {
   const { id } = useParams();
   const [quiz, setQuiz] = useState(null);
+  const [selectedOptions, setSelectedOptions] = useState({});
 
   useEffect(() => {
     const fetchQuiz = async () => {
@@ -17,24 +19,37 @@ function Quiz() {
     fetchQuiz();
   }, [id]);
 
+  const handleOptionClick = (qIndex, oIndex) => {
+    setSelectedOptions((prev) => ({
+      ...prev,
+      [qIndex]: oIndex,
+    }));
+  };
+  
+
   if (!quiz) {
     return <div>Loading...</div>;
   }
 
   return (
     <div>
-      <h1>{quiz.title}</h1>
+      <h1 id='quiz-title'>{quiz.title}</h1>
 
-      <p>{quiz.description}</p>
+      <p id='quiz-description'>{quiz.description}</p>
       
       {quiz.questions && quiz.questions.map((question, qIndex) => (
-        <div key={qIndex}>
-          <h3>{question.question}</h3>
-          <ul>
+        <div id='question-div' key={qIndex}>
+          <h3 className='quiz-question'>{question.question}</h3>
             {question.options && question.options.map((option, oIndex) => (
-              <li key={oIndex}>{option.optionText}</li>
+              <div
+                key={oIndex}
+                onClick={() => handleOptionClick(qIndex, oIndex)}
+                className={selectedOptions[qIndex] === oIndex ? 'selected' : ''}
+                id='answer-div'
+              >
+                {option.optionText}
+              </div>
             ))}
-          </ul>
         </div>
       ))}
     </div>
